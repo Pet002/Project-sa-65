53b344a27c8681884f2fc673cc83981891db4fde
Thanaporn4226 <bambt4226@gmail.com>
2022-10-15 14:33:58 +0700

    medicinelabel

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -1,54 +1,169 @@
+// package entity
+
+// import (
+// 	"github.com/Thanaporn4226/Project-sa-65/services"
+// 	"gorm.io/driver/sqlite"
+// 	"gorm.io/gorm"
+// )
+
+// var db *gorm.DB
+
+// func DB() *gorm.DB {
+// 	return db
+// }
+
+// func SetupDatabase() {
+// 	database, err := gorm.Open(sqlite.Open("MedicineRoom.db"), &gorm.Config{})
+
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
+
+// 	database.AutoMigrate(
+// 		&Login{},
+// 		&Role{},
+// 		&Employee{},
+// 	)
+
+// 	db = database
+
+// 	//เราจะทำการสร้าง Admin account ไว้สำหรับการสร้าง Employee และ role ต่างๆ
+
+// 	password, _ := services.Hash("123456")
+
+// 	role := Role{
+// 		Name: "admin",
+// 	}
+
+// 	login := Login{
+// 		User:     "Admin",
+// 		Password: string(password),
+// 	}
+
+// 	db.Model(&Role{}).Create(&role)
+// 	Loginerr := db.Model(&Login{}).Create(&login)
+// 	if Loginerr.Error == nil {
+// 		db.Model(&Employee{}).Create(&Employee{
+// 			Name:    "Admin",
+// 			Surname: "Admin",
+// 			Login:   login,
+// 			Role:    role,
+// 		})
+// 	}
+
+// }
+
 package entity
 
 import (
-	"github.com/Pet002/Project-sa-65/services"
-	"gorm.io/driver/sqlite"
+	"time"
+
 	"gorm.io/gorm"
+
+	"gorm.io/driver/sqlite"
 )
 
 var db *gorm.DB
 
 func DB() *gorm.DB {
+
 	return db
+
 }
 
 func SetupDatabase() {
-	database, err := gorm.Open(sqlite.Open("MedicineRoom.db"), &gorm.Config{})
+
+	database, err := gorm.Open(sqlite.Open("sa-65.db"), &gorm.Config{})
 
 	if err != nil {
+
 		panic("failed to connect database")
+
 	}
 
+	// Migrate the schema
+
 	database.AutoMigrate(
-		&Login{},
-		&Role{},
-		&Employee{},
+		&MedicineUse{},
+		&Warning{},
+		&MedicineLabel{},
 	)
 
 	db = database
 
-	//เราจะทำการสร้าง Admin account ไว้สำหรับการสร้าง Employee และ role ต่างๆ
-
-	password, _ := services.Hash("123456")
-
-	role := Role{
-		Name: "admin",
+	//Employee Data
+	emp1 := Employee{
+		Name:    "Thanaporn",
+		Surname: "Jansap",
+		//Role:    "Pharmacist",
 	}
+	db.Model(&Employee{}).Create(&emp1)
 
-	login := Login{
-		User:     "Admin",
-		Password: string(password),
+	emp2 := Employee{
+		Name:    "Name",
+		Surname: "Surname",
+		//Role:    "Pharmacist",
 	}
+	db.Model(&Employee{}).Create(&emp2)
 
-	db.Model(&Role{}).Create(&role)
-	Loginerr := db.Model(&Login{}).Create(&login)
-	if Loginerr.Error == nil {
-		db.Model(&Employee{}).Create(&Employee{
-			Name:    "Admin",
-			Surname: "Admin",
-			Login:   login,
-			Role:    role,
-		})
+	emp3 := Employee{
+		Name:    "Manee",
+		Surname: "Jaidee",
+		//Role:    "Pharmacist",
 	}
+	db.Model(&Employee{}).Create(&emp3)
 
+	//MedicineUse Data
+	mu1 := MedicineUse{
+		How_To_Use: "ครั้งละ 1 เม็ด ทุก 4-6 ชั่วโง เวลาปวดหรือมีไข้",
+	}
+	db.Model(&MedicineUse{}).Create(&mu1)
+
+	mu2 := MedicineUse{
+		How_To_Use: "ครั้งละ 1 เม็ด หลังอาหาร เช้า-กลางวัน-เย็น",
+	}
+	db.Model(&MedicineUse{}).Create(&mu2)
+
+	mu3 := MedicineUse{
+		How_To_Use: "ครั้งละ 1 เม็ด ก่อนอาหาร เช้า-กลางวัน-เย็น",
+	}
+	db.Model(&MedicineUse{}).Create(&mu3)
+
+	// Warning Data
+	w1 := Warning{
+		Medicine_Warning: "ห้ามใช้เกิน 8 เม็ดต่อวัน",
+	}
+	db.Model(&Warning{}).Create(&w1)
+
+	w2 := Warning{
+		Medicine_Warning: "ทานยาแล้วอาจรู้สึกง่วงซึม",
+	}
+	db.Model(&Warning{}).Create(&w2)
+
+	w3 := Warning{
+		Medicine_Warning: "ไม่ควรใช้เกินกว่าขนาดที่ระบุ",
+	}
+	db.Model(&Warning{}).Create(&w3)
+
+	//MedicineLabel Data
+	db.Model(&MedicineLabel{}).Create(&MedicineLabel{
+		RecordingDate: time.Now(),
+		Warning:       w1,
+		MedicineUse:   mu1,
+		Employee:      emp1,
+	})
+
+	db.Model(&MedicineLabel{}).Create(&MedicineLabel{
+		RecordingDate: time.Now(),
+		Warning:       w2,
+		MedicineUse:   mu2,
+		Employee:      emp1,
+	})
+
+	db.Model(&MedicineLabel{}).Create(&MedicineLabel{
+		RecordingDate: time.Now(),
+		Warning:       w3,
+		MedicineUse:   mu3,
+		Employee:      emp2,
+	})
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,138 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+//import dayjs, { Dayjs } from 'dayjs';
+import Stack from '@mui/material/Stack';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Test
-        </a>
-      </header>
-    </div>
-  );
+  // export default function MaterialUIPickers() {
+  //   const [value, setValue] = React.useState<Dayjs | null>(
+  //     dayjs('2014-08-18T21:11:54'),
+  //   );
+
+  //   const handleChange = (newValue: Dayjs | null) => {
+  //     setValue(newValue);
+  //   };
+
+
+    return (
+
+      <div>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                ฉลากยา
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+
+        <Container maxWidth="md">
+          <Paper>
+            <Box
+              display={"flex"}
+              sx={{
+                marginTop: 2,
+                paddingX: 1,
+                paddingY: 1,
+              }}
+            >
+              <h2>ฉลากยา</h2>
+            </Box>
+            <hr />
+            <Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs={2}>
+                  <p>วิธีการใช้ยา</p>
+                </Grid>
+                <Grid item xs={7}>
+                  <TextField fullWidth id="วิธีการใช้ยา" variant="outlined" />
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs={2}>
+                  <p>คำเตือน</p>
+                </Grid>
+                <Grid item xs={7}>
+                  <TextField fullWidth id="คำเตือน" variant="outlined" />
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs={2}>
+                  <p>เภสัชกร</p>
+                </Grid>
+                <Grid item xs={7}>
+                  <TextField fullWidth id="เภสัชกร" variant="outlined" />
+                </Grid>
+              </Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs={2}>
+                  <p>วันที่และเวลา</p>
+                </Grid>
+                <Grid item xs={7}>
+                  <TextField fullWidth id="วันที่และเวลา" variant="outlined" />
+                </Grid>
+                {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <Stack spacing={3}>
+                    <DateTimePicker
+                      label="วันที่และเวลา"
+                      value={value}
+                      onChange={handleChange}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Stack>
+                </LocalizationProvider> */}
+              </Grid>
+
+              <Grid container spacing={10}>
+                <Grid item xs={12}>
+                  <Button variant="contained" color="info">
+                    ยกเลิก
+                  </Button>
+                  <Button variant="contained" color="success" sx={{ float: "right" }}>
+                    บันทึกฉลากยา
+                  </Button>
+                </Grid>
+              </Grid>
+
+            </Grid>
+          </Paper>
+        </Container>
+      </div>
+
+
+    );
+  //}
 }
 
 export default App;
