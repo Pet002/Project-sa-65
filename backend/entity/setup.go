99ad7646a72414574be513d071d1fec5ec1f11ce
Pet002 <pet.14336@gmail.com>
2022-10-18 11:52:33 +0700

    add data

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -123,6 +123,20 @@ func SetupDatabase() {
 	if Loginerr4.Error == nil {
 		db.Model(&Employee{}).Create(&emp4)
 	}
+	login_6 := Login{
+		User:     "U6215576",
+		Password: string(password),
+	}
+	Loginer_6 := db.Model(&Login{}).Create(&login_6)
+	if Loginer_6.Error == nil {
+		e1 := Employee{
+			Name:    "Butsakorn",
+			Surname: "Kharom",
+			Login:   login_6,
+			Role:    role,
+		}
+		db.Model(&Employee{}).Create(&e1)
+	}
 
 	role = Role{
 		Name: "payment",
@@ -322,5 +336,119 @@ func SetupDatabase() {
 		MedicineUse:   mu3,
 		Employee:      emp5,
 	})
+
+	var e1 Employee
+	db.Raw("SELECT * FROM employees WHERE name = ?", "Butsakorn").Scan(&e1)
+	role = Role{
+		Name: "intendant",
+	}
+	db.Model(&Role{}).Create(&role)
+	role = Role{
+		Name: "payment",
+	}
+	db.Model(&Role{}).Create(&role)
+
+	//Medicine
+	medicine_1 := Medicine{
+		Name:    "Paracetamol",
+		Type:    med,
+		MFD:     time.Date(2022, 8, 28, 12, 0, 0, 0, time.UTC),
+		EXP:     time.Date(2023, 8, 28, 12, 0, 0, 0, time.UTC),
+		Storage: b1,
+		Amount:  100,
+	}
+	db.Model(&Medicine{}).Create(&medicine_1)
+
+	medicine_2 := Medicine{
+		Name:    "Menopain",
+		Type:    med,
+		MFD:     time.Date(2022, 8, 30, 12, 0, 0, 0, time.UTC),
+		EXP:     time.Date(2023, 8, 30, 12, 0, 0, 0, time.UTC),
+		Storage: b1,
+		Amount:  100,
+	}
+	db.Model(&Medicine{}).Create(&medicine_2)
+
+	medicine_3 := Medicine{
+		Name:    "b-derm",
+		Type:    med,
+		MFD:     time.Date(2022, 8, 30, 12, 0, 0, 0, time.UTC),
+		EXP:     time.Date(2023, 8, 30, 12, 0, 0, 0, time.UTC),
+		Storage: b2,
+		Amount:  100,
+	}
+
+	db.Model(&Medicine{}).Create(&medicine_3)
+
+	medicine_4 := Medicine{
+		Name:    "Cetirizine",
+		Type:    nam,
+		MFD:     time.Date(2022, 10, 24, 12, 0, 0, 0, time.UTC),
+		EXP:     time.Date(2023, 10, 24, 12, 0, 0, 0, time.UTC),
+		Storage: b2,
+		Amount:  100,
+	}
+	db.Model(&Medicine{}).Create(&medicine_4)
+
+	patient_1 := Patient{
+		PID:     "P0001",
+		Name:    "กิตติมากร",
+		Surname: "สอนแก้ว",
+		Age:     21,
+		Gender:  "หญิง",
+		Allergy: "ยาแอสไพริน",
+	}
+	db.Model(&Patient{}).Create(&patient_1)
+
+	patient_2 := Patient{
+		PID:     "P0002",
+		Name:    "ยศพล",
+		Surname: "จันทะนาม",
+		Age:     22,
+		Gender:  "ชาย",
+		Allergy: "ไม่แพ้ยาใดๆ",
+	}
+
+	db.Model(&Patient{}).Create(&patient_2)
+	patient_3 := Patient{
+		PID:     "P0003",
+		Name:    "กฤษฎา",
+		Surname: "น้อยผา",
+		Age:     22,
+		Gender:  "ชาย",
+		Allergy: "ไม่แพ้ยาใดๆ",
+	}
+
+	db.Model(&Patient{}).Create(&patient_3)
+
+	prescription_1 := Prescription{
+		PrescriptionID: "P00001",
+		Symptom:        "ไข้หวัด",
+		Case_Time:      time.Now(),
+		Employee:       e1,
+		Medicine:       medicine_1,
+		Patient:        patient_1,
+	}
+	db.Model(&Prescription{}).Create(&prescription_1)
+
+	prescription_2 := Prescription{
+		PrescriptionID: "P00002",
+		Symptom:        "ปวดหัว",
+		Case_Time:      time.Now(),
+		Employee:       e1,
+		Medicine:       medicine_1,
+		Patient:        patient_2,
+	}
+	db.Model(&Prescription{}).Create(&prescription_2)
+
+	prescription_3 := Prescription{
+		PrescriptionID: "P00003",
+		Symptom:        "แผลถลอก",
+		Case_Time:      time.Now(),
+		Employee:       e1,
+		Medicine:       medicine_3,
+		Patient:        patient_2,
+	}
+	db.Model(&Prescription{}).Create(&prescription_3)
 
 }
